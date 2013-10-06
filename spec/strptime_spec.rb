4de5895b7aa64bfc7f65e0c1a5dca1c760676dc8
Felipe Contreras <felipe.contreras@gmail.com>
2013-10-06 17:03:37 -0500

    spec: strptime: improve '%s %z' test
    We can test both the time and timezone at the same time, and also, it
    makes sense to do the succinct test of checking back-to-back conversion
    of strptime().strftime().
    
    Signed-off-by: Felipe Contreras <felipe.contreras@gmail.com>

--- spec/strptime_spec.rb
+++ spec/strptime_spec.rb
@@ -11,7 +11,8 @@ describe "Time#strptime" do
   end
 
   it "parses number of seconds since Unix Epoch with timezone" do
-    Time.strptime('0 +0100', '%s %z').utc_offset.should == Time.at(0).getlocal('+01:00').utc_offset
+    Time.strptime('0 +0100', '%s %z').to_s.should == Time.at(0).getlocal('+01:00').to_s
+    Time.strptime('0 +0100', '%s %z').strftime('%s %z').should == "0 +0100"
   end
 
 end
